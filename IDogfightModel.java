ad68626b9866570c1dd309471c4820b0eb735024
Johan <Johan@10.133.130.110>
2018-04-23 16:17:12 +0200

    Phase 2

---
+++ IDogfightModel.java
@@ -0,0 +1,19 @@
+
+public interface IDogfightModel {
+
+	public static void IArea(IArea Area) {
+		return null;
+	}
+	
+	public static void IMobile(Imobile mobile) {
+		
+	}
+	
+	public static void Dimension(Dimension dimension) {
+		return null;
+	}
+	
+	public static void Direction(Direction direction) {
+		return null;
+	}
+}
