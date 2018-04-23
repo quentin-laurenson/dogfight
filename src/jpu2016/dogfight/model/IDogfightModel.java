0ef8f1d3e88c9a002cf062736fbb43c636e61235
quentin-laurenson <33093842+quentin-laurenson@users.noreply.github.com>
2018-04-23 17:05:01 +0200

    rebis

---
+++ src/jpu2016/dogfight/model/IDogfightModel.java
@@ -0,0 +1,20 @@
+package jpu2016.dogfight.model;
+
+public interface IDogfightModel {
+
+	public static void IArea(IArea area){
+		return null;
+	}
+	
+	public  static void IMobile(Imobile mobile){
+		return null;
+	}
+	
+	public static void Dimension(Dimension dimension){
+		return null;
+	}
+	
+	public static void Direction(Direction direction){
+		return null;
+	}
+}
