8129f01e1318874108601a82b672663632bd4e8e
quentin-laurenson <33093842+quentin-laurenson@users.noreply.github.com>
2018-04-24 11:07:51 +0200

    phase 3 controller

---
+++ src/jpu2016/dogfight/controller/IUserOrder.java
@@ -0,0 +1,9 @@
+package jpu2016.dogfight.controller;
+
+public interface IUserOrder {
+
+	int getPlayer();
+
+	Order getOrder();
+
+}
---
+++ src/jpu2016/dogfight/controller/UserOrder.java
@@ -0,0 +1,11 @@
+package jpu2016.dogfight.controller;
+
+public class UserOrder {
+	private int player;
+
+	public int getPlayer() {
+		return player;
+	}
+	@override
+	public void getOrder
+}
