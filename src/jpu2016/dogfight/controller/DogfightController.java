dec36a08e631b61545fee3c94d357ea68ecfc2f0
quentin-laurenson <33093842+quentin-laurenson@users.noreply.github.com>
2018-04-24 11:08:20 +0200

    phase 3 controller bis

--- src/jpu2016/dogfight/controller/DogfightController.java
+++ src/jpu2016/dogfight/controller/DogfightController.java
@@ -1,5 +1,29 @@
 package jpu2016.dogfight.controller;
 
-public class DogfightController {
+public class DogfightController implements IOrderPerformer{
+	private int TIME_SLEEP = 30;
+	
+	public DogfightController(IDogfightModel dogfightModel){
+		
+	}
+	/* (non-Javadoc)
+	 * @see jpu2016.dogfight.controller.IOrderPerformer#orderPerformer(jpu2016.dogfight.controller.UserOrder)
+	 */
+	@Override
+	public void orderPerformer(UserOrder userOrder){
+		
+	}
+	public void play(){
+		
+	}
+	public void setViewSystem(IviewSystem viewSystem){
+		
+	}
+	private void lauchMissil(int player){
+		
+	}
+	private void gameLoop(){
+		
+	}
 
 }
--- src/jpu2016/dogfight/controller/UserOrder.java
+++ src/jpu2016/dogfight/controller/UserOrder.java
@@ -1,11 +1,24 @@
 package jpu2016.dogfight.controller;
 
-public class UserOrder {
+public class UserOrder implements IUserOrder {
 	private int player;
 
+	/* (non-Javadoc)
+	 * @see jpu2016.dogfight.controller.IUserOrder#getPlayer()
+	 */
+	@Override
 	public int getPlayer() {
 		return player;
 	}
-	@override
-	public void getOrder
+	/* (non-Javadoc)
+	 * @see jpu2016.dogfight.controller.IUserOrder#getOrder()
+	 */
+	@Override
+	public Order getOrder(){
+		return this.getOrder();
+	}
+	
+	public UserOrder(int player, Order order){
+		
+	}
 }
